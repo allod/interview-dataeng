23c1969c60b207a953c2b69d900b4082a6a561d6
allod <allod7@gmail.com>
2019-06-20 01:06:46 +0100

    Solution for MathService part of the test

--- src/main/scala/io/ctek/interview/dataeng/MathService.scala
+++ src/main/scala/io/ctek/interview/dataeng/MathService.scala
@@ -2,6 +2,8 @@ package io.ctek.interview.dataeng
 
 import cats.data.{Kleisli, ReaderT}
 import cats.instances.try_._
+
+import scala.math.abs
 import scala.util.Try
 
 case class DataEnvironment(data: Set[Int])
@@ -9,18 +11,24 @@ case class DataEnvironment(data: Set[Int])
 class MathService {
   def gcd: ReaderT[Try, DataEnvironment, Either[String, Int]] = for {
     data <- retrieveData
-  } yield data.map(_.reduceLeft(gcd))
+  } yield data.map(_.foldLeft(0)(gcd))
 
   def gcd(a: Int, b: Int): Int = b match {
-    case 0 => a
+    case 0 => abs(a)
     case _ => gcd(b, a % b)
   }
 
   def retrieveData: ReaderT[Try, DataEnvironment, Either[String, Set[Int]]] = Kleisli {
     (env) =>
       Try {
-        if (env.data.size == 1 && env.data.contains(0)) {
+        if (env.data == null) {
+          Left("gcd of null set is undefined")
+        } else if (env.data == Set()){
+          Left("gcd of empty set is undefined")
+        } else if (env.data == Set(0)){
           Left("gcd of 0 is undefined")
+        } else if (env.data == Set(Int.MinValue) || env.data == Set(0, Int.MinValue)){
+          Left("gcd of set where expected result is abs(Int.MinValue) is undefined")
         } else {
           Right(env.data)
         }
--- src/test/scala/io/ctek/interview/dataeng/MathServiceSpec.scala
+++ src/test/scala/io/ctek/interview/dataeng/MathServiceSpec.scala
@@ -16,6 +16,14 @@ class MathServiceSpec extends Specification {
         should return error single value of zero $singleValueZero
         should return correct result for two value $twoValue
         should return correct result for five value $fiveValues
+        should return error result for null value $nullValue
+        should return error result for empty set $noValue
+        should return correct result for two mixed sign values $twoMixedSignValues
+        should return correct result for positive number and zero $twoValuesPositiveAndZero
+        should return correct result for negative number and zero $twoValuesNegativeAndZero
+        should return error result for single negative value $singleNegativeValue
+        should return error result for single integer min value $singleValueIntegerMinimum
+        should return error result for 0 and integer min value $zeroAndIntegerMinimumValues
     """
 
   val mathService = new MathService
@@ -42,6 +50,61 @@ class MathServiceSpec extends Specification {
     val dataEnv = DataEnvironment(Set(25, 35, 75, 145, 90))
     mathService.gcd.run(dataEnv) must beEqualTo(
       Success(Right(5))
+    )
+  }
+
+  def nullValue = {
+    val dataEnv = DataEnvironment(null)
+    mathService.gcd.run(dataEnv) must beEqualTo(
+      Success(Left("gcd of null set is undefined"))
+    )
+  }
+  def noValue = {
+    val dataEnv = DataEnvironment(Set.empty[Int])
+    mathService.gcd.run(dataEnv) must beEqualTo(
+      Success(Left("gcd of empty set is undefined"))
+    )
+  }
+  def twoMixedSignValues = {
+    val dataEnv = DataEnvironment(Set(-15, 25))
+    mathService.gcd.run(dataEnv) must beEqualTo(
+      Success(Right(5))
+    )
+  }
+  def twoValuesPositiveAndZero = {
+    val dataEnv = DataEnvironment(Set(15, 0))
+    mathService.gcd.run(dataEnv) must beEqualTo(
+      Success(Right(15))
+    )
+  }
+  def twoValuesNegativeAndZero = {
+    val dataEnv = DataEnvironment(Set(-15, 0))
+    mathService.gcd.run(dataEnv) must beEqualTo(
+      Success(Right(15))
+    )
+  }
+  def singleNegativeValue = {
+    val dataEnv = DataEnvironment(Set(-4))
+    mathService.gcd.run(dataEnv) must beEqualTo(
+      Success(Right(4))
+    )
+  }
+  def singleValueIntegerMinimum = {
+    val dataEnv = DataEnvironment(Set(Int.MinValue))
+    mathService.gcd.run(dataEnv) must beEqualTo(
+      Success(Left("gcd of set where expected result is abs(Int.MinValue) is undefined"))
+    )
+  }
+  def zeroAndIntegerMinimumValues = {
+    val dataEnv = DataEnvironment(Set(0, Int.MinValue))
+    mathService.gcd.run(dataEnv) must beEqualTo(
+      Success(Left("gcd of set where expected result is abs(Int.MinValue) is undefined"))
+    )
+  }
+  def integerMinimumAndPositiveValue = {
+    val dataEnv = DataEnvironment(Set(Int.MinValue, 14))
+    mathService.gcd.run(dataEnv) must beEqualTo(
+      Success(Right(1))
     )
   }
 }
